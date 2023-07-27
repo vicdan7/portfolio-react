49851ea7715d37fd3b1ac39832b3e39ccf40c7c0
vicdan7 <ibarravictor334@gmail.com>
2023-07-27 14:31:28 -0500

    cv

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Info from '../../components/Info';
 import Stats from '../../components/Stats';
 import {FaDownload} from "react-icons/fa"
-import CV from "../../assets/CV VICTOR IBARRA.pdf"
+import CV from "../../assets/CV VICTOR.pdf"
 import Skills from '../../components/Skills';
 import { resume } from '../../data'
 import ResumeItem from '../../components/ResumeItem';
