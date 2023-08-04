51588415bf60cfb607f301dea436a52b27b5b7a6
vicdan7 <ibarravictor334@gmail.com>
2023-08-04 14:25:54 -0500

    emails

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import{
+import React, { useState } from "react";
+import {
   FaEnvelopeOpen,
   FaPhoneSquareAlt,
   FaFacebookF,
@@ -7,110 +7,180 @@ import{
   FaGithub,
   FaLinkedin,
   FaWhatsapp,
-} from 'react-icons/fa';
+} from "react-icons/fa";
 
-import { FiSend } from 'react-icons/fi';
+import { FiSend } from "react-icons/fi";
 
-import "./contact.css"
+import "./contact.css";
 
-const Contact = () => {
+const sendEmail = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch(`${import.meta.env.VITE_API_URL}/emails/contact`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setFormData({
+          name: "",
+          email: "",
+          subject: "",
+          message: "",
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-   <section className="contact section">
-     <h2 className="section__title">Get In <span>Touch</span></h2>
+    <section className="contact section">
+      <h2 className="section__title">
+        Get In <span>Touch</span>
+      </h2>
 
-     <div className="contact__container container grid">
-       <div className="contact__data">
-         <h3 className="contact__title">Don't be Shy !!</h3>
+      <div className="contact__container container grid">
+        <div className="contact__data">
+          <h3 className="contact__title">Don't be Shy !!</h3>
 
-         <p className="contact__description">Feel free to get in touch with me. 
-            I am always open to discussing new projects, creative ideas or opportunities
-            to be part of your visions.
-         </p>
+          <p className="contact__description">
+            Feel free to get in touch with me. I am always open to discussing
+            new projects, creative ideas or opportunities to be part of your
+            visions.
+          </p>
 
-         <div className="contact__info">
-           <div className="info__item">
-              <FaEnvelopeOpen className='info__icon' />
+          <div className="contact__info">
+            <div className="info__item">
+              <FaEnvelopeOpen className="info__icon" />
 
               <div>
                 <span className="info__title">Mail me</span>
                 <h4 className="info__desc">ibarravictor334@gmail.com</h4>
               </div>
-           </div>
+            </div>
 
-           <div className="info__item">
-              <FaPhoneSquareAlt className='info__icon' />
+            <div className="info__item">
+              <FaPhoneSquareAlt className="info__icon" />
 
               <div>
                 <span className="info__title">Call me</span>
                 <h4 className="info__desc">+57 3013532321</h4>
                 <FaWhatsapp />
               </div>
-           </div>
-         </div>
+            </div>
+          </div>
 
-         <div className="contact__socials">
-           <a href="https://www.facebook.com/victor.ibarra.5496683?mibextid=9R9pXO" target='__blank' className="contact__social-link">
+          <div className="contact__socials">
+            <a
+              href="https://www.facebook.com/victor.ibarra.5496683?mibextid=9R9pXO"
+              target="__blank"
+              className="contact__social-link"
+            >
               <FaFacebookF />
-           </a>
+            </a>
 
-           <a href="https://github.com/vicdan7" target='__blank' className="contact__social-link">
+            <a
+              href="https://github.com/vicdan7"
+              target="__blank"
+              className="contact__social-link"
+            >
               <FaGithub />
-           </a>
+            </a>
 
-           <a href="https://instagram.com/victoribarrab?igshid=MmU2YjMzNjRlOQ==" target='__blank' className="contact__social-link">
+            <a
+              href="https://instagram.com/victoribarrab?igshid=MmU2YjMzNjRlOQ=="
+              target="__blank"
+              className="contact__social-link"
+            >
               <FaInstagram />
-           </a>
+            </a>
 
-           <a href="https://www.linkedin.com/in/vicibarra/" target='__blank' className="contact__social-link">
+            <a
+              href="https://www.linkedin.com/in/vicibarra/"
+              target="__blank"
+              className="contact__social-link"
+            >
               <FaLinkedin />
-           </a>
-         </div>
-       </div>
+            </a>
+          </div>
+        </div>
 
-       <form className="contact__form">
+        <form className="contact__form" onSubmit={handleSubmit}>
           <div className="form__input-group">
             <div className="form__input-div">
-              <input 
-                type="text" 
-                placeholder='Your Name' 
-                className="form__control" 
+              <input
+                name="name"
+                type="text"
+                placeholder="Your Name"
+                className="form__control"
+                value={formData.name}
+                onChange={handleChange}
               />
             </div>
 
             <div className="form__input-div">
-              <input 
-                type="email" 
-                placeholder='Your Email' 
-                className="form__control" 
+              <input
+                name="email"
+                type="email"
+                placeholder="Your Email"
+                className="form__control"
+                value={formData.email}
+                onChange={handleChange}
               />
             </div>
 
             <div className="form__input-div">
-              <input 
-                type="text" 
-                placeholder='Your Subject' 
-                className="form__control" 
+              <input
+                name="subject"
+                type="text"
+                placeholder="Your Subject"
+                className="form__control"
+                value={formData.subject}
+                onChange={handleChange}
               />
             </div>
           </div>
 
           <div className="form__input-div">
-            <textarea 
-            placeholder='Your Message' 
-            className="form__control textarea"
+            <textarea
+              name="message"
+              placeholder="Your Message"
+              className="form__control textarea"
+              value={formData.message}
+              onChange={handleChange}
             ></textarea>
           </div>
 
-          <button className='button'>
+          <button className="button">
             Send Message
-            <span className='button__icon contact__button-icon'>
+            <span className="button__icon contact__button-icon">
               <FiSend />
             </span>
           </button>
-       </form>
-     </div>
-   </section>
+        </form>
+      </div>
+    </section>
   );
-}
+};
 
-export default Contact;
+export default sendEmail;
